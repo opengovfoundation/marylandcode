20fb80bdcaa0def9fc78d370fee490d5dff15c33
Chris Birk <cmbirk@gmail.com>
2013-05-09 12:47:47 -0400

    send contactable emails through sendgrid

--- htdocs/contactable.1.3/mail.php
+++ htdocs/contactable.1.3/mail.php
@@ -1,4 +1,29 @@
 <?php
+	//Use SendGrid for mailing
+	require_once("sendgrid-php/SendGrid_loader.php");
+
+	function email($em, $subject, $message = '') 
+	{
+
+		try{
+			$sendgrid = new SendGrid('', '');
+
+			$mail = new SendGrid\Mail();
+			$mail->addTo($em)
+				->setFrom('sayhello@opengovfoundation.org')
+				->setSubject($subject)
+				->setText($message);
+
+			$sendgrid->smtp->send($mail);
+		}	
+		catch(Exception $e){
+			error_log($e);
+			return false;
+		}
+
+		return true;
+	}
+
 	//declare our assets 
 	$name = stripcslashes($_POST['name']);
 	$emailAddr = stripcslashes($_POST['email']);
@@ -14,7 +39,12 @@
 		Sending IP:$_SERVER[REMOTE_ADDR]
 		Sending Script: $_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]";
 		
+		
+		
 		//send the email 
-		mail('sayhello@opengovfoundation.org', $subject, $contactMessage);
+		email('sayhello@opengovfoundation.org', $subject, $contactMessage);
 		echo('success'); //return success callback
+		
+		
+		
 ?>
