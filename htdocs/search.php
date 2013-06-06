83b0ec6abcf25d83fab66eba87ac49133a71c32b
Chris Birk <cmbirk@gmail.com>
2013-06-06 13:11:31 -0400

    update search page links to permalink structure

--- htdocs/search.php
+++ htdocs/search.php
@@ -3,9 +3,9 @@
 
 	require $_SERVER['DOCUMENT_ROOT'].'/../includes/page-head.inc.php';
 
+	$query = $_GET['q'];
+	$query_readable = str_replace('-', ' ', $query);
 	
-	$query_readable = str_replace('-', ' ', $_GET['q']);
-	$query = urlencode($query_readable);
 		
 	if(isset($_GET['p'])){
 		$page = $_GET['p'];
@@ -34,17 +34,17 @@
 	$start_page = $page - 5 > 0 ? $page - 5 : 0;
 
 	if(isset($_GET['p'])){
-		$pager_prev_page = ($page - 1) > 1 ? '&p=' . ($page - 1) : '';
-		$pager_prev = '<a href="/search/?q=' . $query . $pager_prev_page . '" rel="prevstart" class="pager-prev">&lt;</a>';
+		$pager_prev_page = ($page - 1) > 1 ? '?p=' . ($page - 1) : '';
+		$pager_prev = '<a href="/term/' . $query . '/' . $pager_prev_page . '" rel="prevstart" class="pager-prev">&lt;</a>';
 		if($page == $pages - 1){
 			$pager_next = '<span class="pager-disabled pager-next">&gt;</span>';
 		}else{
-			$pager_next = '<a href="/search/?q=' . $query . '&p=' . ($page + 1) . '" rel="next" class="pager-next">&gt;</a>';
+			$pager_next = '<a href="/term/' . $query . '/?p=' . ($page + 1) . '" rel="next" class="pager-next">&gt;</a>';
 		}
 	}else{
 		$pager_prev = '<span class="pager-disabled pager-prev">&lt;</span>';
 		if($pages > 1){
-			$pager_next = '<a href="/search/?q=' . $query . '&p=2" rel="next" class="pager-next">&gt;</a>';
+			$pager_next = '<a href="/term/' . $query . '/?p=2" rel="next" class="pager-next">&gt;</a>';
 		}else{
 			$pager_next = '<span class="pager-disabled pager-next">&gt;</span>';
 		}
@@ -69,9 +69,9 @@
 		elseif($i == 0 && !isset($page)){//If this is the first page
 			$body .= '<li><span class="pager-disabled">' . ($i + 1) . '</span></li>';
 		}elseif($i == 0){//Don't append &p query for first page
-			$body .= '<li><a href="/search/?q=' . $query . '" rel="prevstart">' . ($i + 1) . '</a></li>';
+			$body .= '<li><a href="/term/' . $query . '/" rel="prevstart">' . ($i + 1) . '</a></li>';
 		}else{//Create other page links
-			$body .= '<li><a href="/search/?q=' . $query . '&p=' . ($i + 1) . '" rel="prevstart">' . ($i + 1) . '</a></li>';
+			$body .= '<li><a href="/term/' . $query . '/?p=' . ($i + 1) . '" rel="prevstart">' . ($i + 1) . '</a></li>';
 		}
 	}		
 	
