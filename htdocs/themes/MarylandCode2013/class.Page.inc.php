39ffc41f556c1d37246b1c7b6af50d1156d76ead
Bill Hunt <bill@krues8dr.com>
2016-06-30 13:19:09 -0400

    Adding message that site isn't up to date.

--- htdocs/themes/MarylandCode2013/class.Page.inc.php
+++ htdocs/themes/MarylandCode2013/class.Page.inc.php
@@ -28,6 +28,11 @@ class MarylandCode2013__Page extends Page
 			'type' => 'css',
 			'requires' => array('font_awesome_css')
 		),
+		'banner_css' => array(
+			'path' => '/css/banner.css',
+			'type' => 'css',
+			'requires' => array('main_css')
+		),
 		'jquery_ui_css' => array(
 			'path' => '//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css',
 			'type' => 'css',
--- htdocs/themes/MarylandCode2013/default.inc.php
+++ htdocs/themes/MarylandCode2013/default.inc.php
@@ -26,6 +26,9 @@
 	<![endif]-->
   <div id="container"><!-- Hack for footer lock -->
 	<header id="page_header">
+		<div class="banner">
+			This site is out of date and no longer maintained. Please refer to official sources for the up to date legal code.
+		</div>
 		<div class="nest">
 			<a href="/" class="noprint">
 				<hgroup id="place_logo">
